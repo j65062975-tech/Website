3e303df233749109e87390ea9724aa898eb6c85a
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2026-02-22 11:47:39 +0100

    temp images

--- products.js
+++ products.js
@@ -2,13 +2,13 @@
 let products = [
 
     // peptides
-    { id: 1, name: "GenPower Peptide-A (500 mg)", price: 59, category: "Peptide", image: "images/peptide-a.jpg" },
-    { id: 2, name: "GenPower Peptide-B (300 mg)", price: 49, category: "Peptide", image: "images/peptide-b.jpg" },
+    { id: 1, name: "GenPower Peptide-A (500 mg)", price: 59, category: "Peptide", image: "img/products/29-scaled.jpg" },
+    { id: 2, name: "GenPower Peptide-B (300 mg)", price: 49, category: "Peptide", image: "img/products/29-scaled.jpg" },
 
     // zycare supplementen
-    { id: 3, name: "Zycare Vitamin-C 1000 mg", price: 29, category: "Supplement", image: "images/vitamin-c.jpg" },
-    { id: 4, name: "Zycare Omega-3 1200 mg", price: 39, category: "Supplement", image: "images/omega-3.jpg" },
+    { id: 3, name: "Zycare Vitamin-C 1000 mg", price: 29, category: "Supplement", image: "img/products/29-scaled.jpg" },
+    { id: 4, name: "Zycare Omega-3 1200 mg", price: 39, category: "Supplement", image: "img/products/29-scaled.jpg" },
 
     // bundel
-    { id: 5, name: "GenPower Starter Pack (Peptide-A + Vitamin-C)", price: 85, category: "Bundel", image: "images/starter-pack.jpg" }
+    { id: 5, name: "GenPower Starter Pack (Peptide-A + Vitamin-C)", price: 85, category: "Bundel", image: "img/products/29-scaled.jpg" }
 ];
