f235bc1de9b9b567427af26f9d808d581525df88
j65062975-tech <j65062975@gmail.com>
2026-03-08 04:19:34 -0700

    commit

--- products.js
+++ products.js
@@ -1,14 +1,34 @@
 // producten
 let products = [
 
-    // injectie sectie
-    { id: 1, name: "DROSTA-ZYLONE 100mg", price: "39,95", category: "inject", image: "/img/products/DROSTA-ZYLONE-100mg.jpg", images: ["/img/products/DROSTA-ZYLONE-100mg.jpg", "/img/products/DROSTA-ZYLONE-100mg-1.jpg"] },
-    { id: 2, name: "NANDRO-ZYNOATE 250mg", price: "39,95", category: "inject", image: "/img/products/NANDRO-ZYNOATE-250mg.jpg", images: ["/img/products/NANDRO-ZYNOATE-250mg.jpg", "/img/products/NANDRO-ZYNOATE-250mg-1.jpg"] },
-    { id: 3, name: "Primozycare 100mg/ml", price: "44,95", category: "inject", image: "/img/products/Primozycare-100mg.jpg", images: ["/img/products/Primozycare-100mg.jpg", "/img/products/Primozycare-100mg-1.jpg"] },
-    { id: 4, name: "TESTO ZYPRO 100mg", price: "39,95", category: "inject", image: "/img/products/TESTO-ZYPRO-100mg.jpg", images: ["/img/products/TESTO-ZYPRO-100mg.jpg", "/img/products/TESTO-ZYPRO-100mg-1.jpg"] },
-    { id: 5, name: "TESTO-ZYCYP 250mg", price: "39,95", category: "inject", image: "/img/products/TESTO-ZYCYP-250mg.jpg", images: ["/img/products/TESTO-ZYCYP-250mg.jpg", "/img/products/TESTO-ZYCYP-250mg-1.jpg"] },
-    { id: 6, name: "Testo-Zyenate 250mg", price: "39,95", category: "inject", image: "/img/products/Testo-Zyenate-250mg.jpg", images: ["/img/products/Testo-Zyenate-250mg.jpg", "/img/products/Testo-Zyenate-250mg-1.jpg"] },
-    { id: 7, name: "TESTO-ZYMIX 250mg", price: "43,95", category: "inject", image: "/img/products/TESTO-ZYMIX-250mg.jpg", images: ["/img/products/TESTO-ZYMIX-250mg.jpg", "/img/products/TESTO-ZYMIX-250mg-1.jpg"] },
-    { id: 8, name: "TREN-ZYACE 100mg", price: "39,95", category: "inject", image: "/img/products/TREN-ZYACE-100mg.jpg", images: ["/img/products/TREN-ZYACE-100mg.jpg", "/img/products/TREN-ZYACE-100mg-1.jpg"] },
-    { id: 9, name: "NANDRO-ZYPRO 100mg", price: "39,95", category: "inject", image: "/img/products/NANDRO-ZYPRO-100mg.jpg", images: ["/img/products/NANDRO-ZYPRO-100mg.jpg", "/img/products/NANDRO-ZYPRO-100mg-1.jpg"] }
+    // injections - Zycare brand
+    { id: 1, name: "DROSTA-ZYLONE 100mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/DROSTA-ZYLONE-100mg.jpg", images: ["/img/products/DROSTA-ZYLONE-100mg.jpg", "/img/products/DROSTA-ZYLONE-100mg-1.jpg"] },
+    { id: 2, name: "NANDRO-ZYNOATE 250mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/NANDRO-ZYNOATE-250mg.jpg", images: ["/img/products/NANDRO-ZYNOATE-250mg.jpg", "/img/products/NANDRO-ZYNOATE-250mg-1.jpg"] },
+    { id: 3, name: "Primozycare 100mg/ml", price: "44,95", category: "injections", brand: "Zycare", image: "/img/products/Primozycare-100mg.jpg", images: ["/img/products/Primozycare-100mg.jpg", "/img/products/Primozycare-100mg-1.jpg"] },
+    { id: 4, name: "TESTO ZYPRO 100mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/TESTO-ZYPRO-100mg.jpg", images: ["/img/products/TESTO-ZYPRO-100mg.jpg", "/img/products/TESTO-ZYPRO-100mg-1.jpg"] },
+    { id: 5, name: "TESTO-ZYCYP 250mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/TESTO-ZYCYP-250mg.jpg", images: ["/img/products/TESTO-ZYCYP-250mg.jpg", "/img/products/TESTO-ZYCYP-250mg-1.jpg"] },
+    { id: 6, name: "Testo-Zyenate 250mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/Testo-Zyenate-250mg.jpg", images: ["/img/products/Testo-Zyenate-250mg.jpg", "/img/products/Testo-Zyenate-250mg-1.jpg"] },
+    { id: 7, name: "TESTO-ZYMIX 250mg", price: "43,95", category: "injections", brand: "Zycare", image: "/img/products/TESTO-ZYMIX-250mg.jpg", images: ["/img/products/TESTO-ZYMIX-250mg.jpg", "/img/products/TESTO-ZYMIX-250mg-1.jpg"] },
+    { id: 8, name: "TREN-ZYACE 100mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/TREN-ZYACE-100mg.jpg", images: ["/img/products/TREN-ZYACE-100mg.jpg", "/img/products/TREN-ZYACE-100mg-1.jpg"] },
+    { id: 9, name: "NANDRO-ZYPRO 100mg", price: "39,95", category: "injections", brand: "Zycare", image: "/img/products/NANDRO-ZYPRO-100mg.jpg", images: ["/img/products/NANDRO-ZYPRO-100mg.jpg", "/img/products/NANDRO-ZYPRO-100mg-1.jpg"] },
+
+    // tablets - Zycare brand
+    { id: 10, name: "Zycare-Drol 50mg", price: "24,95", category: "tablets", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 11, name: "Zycare-Var 50mg", price: "29,95", category: "tablets", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 12, name: "Zycare-Win 10mg", price: "19,95", category: "tablets", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 13, name: "Zycare-Tbol 25mg", price: "22,95", category: "tablets", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 14, name: "Zycare-Dian 50mg", price: "27,95", category: "tablets", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+
+    // peptides - Zycare brand
+    { id: 15, name: "Zycare-HGH 10IU", price: "54,95", category: "peptides", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 16, name: "Zycare-IGF-1 1mg", price: "49,95", category: "peptides", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 17, name: "Zycare-GHRP-6 5mg", price: "34,95", category: "peptides", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 18, name: "Zycare-CJC-1295 2mg", price: "39,95", category: "peptides", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 19, name: "Zycare-Ipamorelin 5mg", price: "44,95", category: "peptides", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+
+    // libido - Zycare brand
+    { id: 20, name: "Zycare-Libido Boost", price: "34,95", category: "libido", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 21, name: "Zycare-Vigor Max", price: "39,95", category: "libido", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 22, name: "Zycare-Performance Plus", price: "44,95", category: "libido", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] },
+    { id: 23, name: "Zycare-Stamina Formula", price: "37,95", category: "libido", brand: "Zycare", image: "/img/products/placeholder.jpg", images: ["/img/products/placeholder.jpg"] }
 ];
