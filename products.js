86f1c2fdf3b00d44c09ef5f3315367324d5306e5
j65062975-tech <j65062975@gmail.com>
2026-03-18 01:00:40 -0700

    h

--- products.js
+++ products.js
@@ -1,1 +1,266 @@
+// producten
+let products = [
+  // ===== INJECTIONS - ZYCARE BRAND =====
+  {
+    id: 1,
+    name: "Masteron Propionate 100mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "coming soon",
+    image: "img/products/masteron front side view.jpg",
+    images: [
+      "img/products/masteron front side view.jpg",
+      "img/products/masteron back side view.jpg"
+    ]
+  },
+  {
+    id: 2,
+    name: "Deca 250mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "available",
+    image: "img/products/nanrdolone deca front view vials.jpg",
+    images: [
+      "img/products/nanrdolone deca front view vials.jpg",
+      "img/products/nandrolone deca back side view.jpg"
+    ]
+  },
+  {
+    id: 4,
+    name: "Testosteron Propionate 100mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/test p front view vials.jpg",
+    images: [
+      "/img/products/test p front view vials.jpg",
+      "/img/products/test p front view.jpg",
+      "/img/products/test p vials front view.jpg"
+    ]
+  },
+  {
+    id: 5,
+    name: "Testosteron Cypionate 250mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
 
+    HEAD,
+    availability: "available",
+
+
+    availability: "avalable",
+
+    image: "/img/products/test c front view vials 2.jpg",
+    images: [
+      "/img/products/test c front view vials 2.jpg",
+      "/img/products/test c front view vials.jpg",
+      "/img/products/test c front view.jpg"
+    ]
+  },
+  {
+    id: 6,
+    name: "Testosteron Enanthate 250mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/test e front view.jpg",
+    images: [
+      "/img/products/test  back ide view.jpg",
+      "/img/products/test e front side view.jpg",
+      "/img/products/test e front view.jpg"
+    ]
+  },
+  {
+    id: 7,
+    name: "Boldenon 250mg",
+    price: "43,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "available",
+    image: "./img/products/Boldenone front view.jpeg",
+    images: [
+      "./img/products/Boldenone front view.jpeg",
+      "./img/products/Boldenone front view 3.jpeg"
+    ]
+  },
+  {
+    id: 8,
+    name: "Trenbolon Enanthate 100mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/tren e front view.jpg",
+    images: [
+      "/img/products/tren e front view.jpg",
+      "/img/products/tren e vial front viwe.jpg",
+      "/img/products/tren e vials .jpg",
+      "/img/products/tren e backage back side view.jpg"
+    ]
+  },
+  {
+    id: 9,
+    name: "NPP 100mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/nandrolone front side view.jpg",
+    images: [
+      "/img/products/nandrolone front side view.jpg",
+      "/img/products/nandrolone back side view.jpg"
+    ]
+  },
+
+  // ===== TABLETS - ZYCARE BRAND =====
+  {
+    id: 12,
+    name: "Winstrol 10mg",
+    price: "30,00",
+    category: "tablets",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/winstrtol front side view.jpg",
+    images: [
+      "/img/products/winstrtol front side view.jpg"
+    ]
+  },
+  {
+    id: 14,
+    name: "Anadrol 50mg",
+    price: "39,50",
+    category: "tablets",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/oxymethelone front side view.jpg",
+    images: [
+      "/img/products/oxymethelone front side view.jpg",
+      "/img/products/oxymethelone back side view.jpg"
+    ]
+  },
+  {
+    id: 15,
+    name: "Proviron 25mg",
+    price: "45,00",
+    category: "tablets",
+    brand: "Zycare",
+    availability: "available",
+    image: "/img/products/provicare front side view.jpg",
+    images: [
+      "/img/products/provion back side 25 mg.jpg",
+      "/img/products/iron-2.jpg",
+      "/img/products/proviron-3.jpg",
+      "/img/products/provicare front side view.jpg"
+    ]
+  },
+
+  // ===== LIBIDO - ZYCARE BRAND =====
+  {
+    id: 20,
+    name: "Cenforce-100 mg",
+    price: "10,00",
+    category: "libido",
+    brand: "Zycare",
+    availability: "available",
+    description: "1 strip costs 10 euro's",
+    image: "/img/products/Cenforce 100 mg.jpeg",
+    images: [
+      "/img/products/Cenforce 100 mg.jpeg"
+    ]
+  },
+  {
+    id: 21,
+    name: "Cenforce-D 100 + 60mg",
+    price: "10,00",
+    category: "libido",
+    brand: "Zycare",
+    availability: "available",
+    description: "1 strip costs 10 euro's",
+    image: "/img/products/Cenforce d.jpeg",
+    images: [
+      "/img/products/Cenforce d.jpeg"
+    ]
+  },
+  {
+    id: 22,
+    name: "Careforce FM 100 mg",
+    price: "10,00",
+    category: "libido",
+    brand: "Zycare",
+    availability: "available",
+    description: "1 strip costs 10 euro's",
+    image: "/img/products/careforce fm.jpeg",
+    images: [
+      "/img/products/careforce fm.jpeg"
+    ]
+  },
+  {
+    id: 24,
+    name: "Cenforce 200 mg",
+    price: "10,00",
+    category: "libido",
+    brand: "Zycare",
+    availability: "available",
+    description: "1 strip costs 10 euro's",
+    image: "/img/products/Cenforce 200.jpeg",
+    images: [
+      "/img/products/Cenforce 200.jpeg",
+      "/img/products/Cenforce 200 mg 2.jpeg"
+    ]
+  },
+
+  // ===== NEW PRODUCTS =====
+  {
+    id: 33,
+    name: "Anavar 10mg",
+    price: "44,95",
+    category: "tablets",
+    brand: "Genetic Life Science",
+    availability: "coming soon",
+    image: "/img/products/dxvqunmlnr-1668635636.jpg",
+    images: [
+      "/img/products/dxvqunmlnr-1668635636.jpg"
+    ]
+  },
+  {
+    id: 34,
+    name: "Trenbolone Acetate 100mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Genetic Life Science",
+    availability: "coming soon",
+    image: "/img/products/m4a9hyem95-1668548005.jpg",
+    images: [
+      "/img/products/m4a9hyem95-1668548005.jpg"
+    ]
+  },
+  {
+    id: 35,
+    name: "Turinabol 25mg",
+    price: "35,00",
+    category: "tablets",
+    brand: "Genetic Life Science",
+    availability: "coming soon",
+    image: "/img/products/snyejvzz4o-1668708435.jpg",
+    images: [
+      "/img/products/snyejvzz4o-1668708435.jpg"
+    ]
+  },
+  {
+    id: 36,
+    name: "Masteron propionate 100mg",
+    price: "39,95",
+    category: "injections",
+    brand: "Genetic Life Science",
+    availability: "available",
+    image: "/img/products/i9vt5cv6kf-1668531489.jpg",
+    images: [
+      "/img/products/i9vt5cv6kf-1668531489.jpg"
+    ]
+  }
+];
